2b1044c1e6fd15c524a2366c9a881cf352292c91
Oleg Makeienko <molva83@gmail.com>
2024-02-02 11:06:28 +0100

    created ChatRoomRepo without some extra method

---
+++ src/main/java/com/makeienko/restcrudmedjpa/repository/ChatRoomRepo.java
@@ -0,0 +1,7 @@
+package com.makeienko.restcrudmedjpa.repository;
+
+import com.makeienko.restcrudmedjpa.model.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepo extends JpaRepository<ChatRoom, Long > {
+}
