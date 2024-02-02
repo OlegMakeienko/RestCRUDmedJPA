b9142c637bd216137dca3e70461916c744d5a269
Oleg Makeienko <molva83@gmail.com>
2024-02-02 10:42:58 +0100

    created model: class ChatRoom (Entity)

--- src/main/java/com/makeienko/restcrudmedjpa/model/ChatRoom.java
+++ src/main/java/com/makeienko/restcrudmedjpa/model/ChatRoom.java
@@ -1,4 +1,26 @@
 package com.makeienko.restcrudmedjpa.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    public ChatRoom() {
+    }
+
+    public ChatRoom(String title) {
+        this.title = title;
+    }
 }
