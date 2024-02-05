35f546877892e5b519af1fdd929c08f2924c59eb
Oleg Makeienko <molva83@gmail.com>
2024-02-05 20:06:42 +0100

    created class entity Message

---
+++ src/main/java/com/makeienko/restcrudmedjpa/model/Message.java
@@ -0,0 +1,19 @@
+package com.makeienko.restcrudmedjpa.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+}
