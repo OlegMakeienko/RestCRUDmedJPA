18a3e1967512da65885085b4540ec9556dcdc328
Oleg Makeienko <molva83@gmail.com>
2024-02-08 09:21:28 +0100

    Generetion Type changed to Auto, Getters & Setters

--- src/main/java/com/makeienko/restcrudmedjpa/model/Message.java
+++ src/main/java/com/makeienko/restcrudmedjpa/model/Message.java
@@ -1,13 +1,17 @@
 package com.makeienko.restcrudmedjpa.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "messages")
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
