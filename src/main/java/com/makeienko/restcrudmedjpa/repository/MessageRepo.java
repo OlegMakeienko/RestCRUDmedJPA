1b4c74531467a046ef9dff9a7398f186fdaee2dd
Oleg Makeienko <molva83@gmail.com>
2024-02-08 14:11:44 +0100

    delete unnecessary method and import

--- src/main/java/com/makeienko/restcrudmedjpa/repository/MessageRepo.java
+++ src/main/java/com/makeienko/restcrudmedjpa/repository/MessageRepo.java
@@ -2,6 +2,7 @@ package com.makeienko.restcrudmedjpa.repository;
 
 import com.makeienko.restcrudmedjpa.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -10,9 +11,11 @@ public interface MessageRepo extends JpaRepository<Message, Long> {
 
     @Query("SELECT m FROM Message m WHERE m.chatRoom.id = :id")
     List<Message> findByChannelId(Long id);
-//
-//    void deleteById(Long id, Long messageId);
 
     @Query("SELECT m FROM Message m WHERE m.id = :messageId AND m.chatRoom.id = :channelId")
     Message findByIdAndChannelId(Long messageId, Long channelId);
+
+//    @Modifying
+//    @Query("DELETE FROM Message m WHERE m.id = :messageId AND m.chatRoom.id = :channelId")
+//    Message deleteByIdAndChannelId(Long messageId, Long id);
 }
