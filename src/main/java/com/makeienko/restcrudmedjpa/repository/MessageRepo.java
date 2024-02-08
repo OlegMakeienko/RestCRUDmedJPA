054c7b065b23484f1bd0f2277c8b2b64498f41e0
Oleg Makeienko <molva83@gmail.com>
2024-02-08 13:50:32 +0100

    massage findByIdAndChannelId method created i repo

--- src/main/java/com/makeienko/restcrudmedjpa/repository/MessageRepo.java
+++ src/main/java/com/makeienko/restcrudmedjpa/repository/MessageRepo.java
@@ -3,7 +3,6 @@ package com.makeienko.restcrudmedjpa.repository;
 import com.makeienko.restcrudmedjpa.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +10,9 @@ public interface MessageRepo extends JpaRepository<Message, Long> {
 
     @Query("SELECT m FROM Message m WHERE m.chatRoom.id = :id")
     List<Message> findByChannelId(Long id);
+//
+//    void deleteById(Long id, Long messageId);
 
+    @Query("SELECT m FROM Message m WHERE m.id = :messageId AND m.chatRoom.id = :channelId")
+    Message findByIdAndChannelId(Long messageId, Long channelId);
 }
