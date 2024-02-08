4775c45a1db4ff3c13b98b07176338d90fab0d6f
Oleg Makeienko <molva83@gmail.com>
2024-02-08 09:20:45 +0100

    Generetion Type changed to Auto

--- src/main/java/com/makeienko/restcrudmedjpa/model/ChatRoom.java
+++ src/main/java/com/makeienko/restcrudmedjpa/model/ChatRoom.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class ChatRoom {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
 
